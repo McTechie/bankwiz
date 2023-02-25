1690799b5161c1eb800750a68039edc36e0f3f93
MayankBhate <bhatemayank64@gmail.com>
2023-02-25 16:57:32 +0530

    Make migrations

--- server/company_api/urls.py
+++ server/company_api/urls.py
@@ -2,6 +2,6 @@ from .views import Companys as all_view
 from .views import SingleCompanyCreator as single_creator_view
 from .views import SingleCompanyOperations as single_operations_view
 
-from utils.general_utils import get_url_patterns
+from bankwiz.server.utils.general_utils import get_url_patterns
 
 urlpatterns = get_url_patterns('company', all_view, single_creator_view, single_operations_view)
