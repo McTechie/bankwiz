e45e31e50fd11974fc54cb2c22d7f992f5b83824
gluedtokeyboard <80961962+gluedtokeyboard@users.noreply.github.com>
2023-02-25 16:55:15 +0530

    Add urls

--- server/server/urls.py
+++ server/server/urls.py
@@ -14,8 +14,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("api/", include("account_api.urls")),
+    path("api/", include("bank_api.urls")),
+    path("api/", include("company_api.urls")),
+    path("api/", include("transaction_api.urls")),
+    path("api/", include("user_api.urls")),
 ]
