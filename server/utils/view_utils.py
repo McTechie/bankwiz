e99ad2917c5ed47801a23078b778112d5fe5d0f1
gluedtokeyboard <80961962+gluedtokeyboard@users.noreply.github.com>
2023-02-25 16:58:59 +0530

    Modify imports

--- server/utils/view_utils.py
+++ server/utils/view_utils.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from bankwiz.server.utils.general_utils import exception_catcher
+from utils.general_utils import exception_catcher
 
 
 def collective_operations(model, serializer):
