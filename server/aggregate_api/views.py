9412ace583615c8dc96cfb788f81e1d52e4367cb
gluedtokeyboard <80961962+gluedtokeyboard@users.noreply.github.com>
2023-02-26 06:35:52 +0530

    Add get transactions by user id

--- server/aggregate_api/views.py
+++ server/aggregate_api/views.py
@@ -3,9 +3,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 
-from .models import Log
-from server.transaction_api.models import Transaction
-from server.transaction_api.serializer import TransactionUserSerializer
+from transaction_api.models import Transaction
+from user_api.models import User
+from transaction_api.serializer import TransactionUserSerializer
 from utils.general_utils import exception_catcher
 
 # Create your views here.
@@ -14,12 +14,17 @@ class UserTransactions(APIView):
     @exception_catcher
     def get(self, request):
         user_id = request.data['user_id']
-        all_transactions = Transaction.objects.all()
-        transactions = []
-        for transaction in all_transactions:
-            pass
-
+        user = User.objects.filter(user_id=user_id)[0]
+        company_id = user.company_id
+        transactions = Transaction.objects.filter(company_id=company_id)
+        serialized = TransactionUserSerializer(transactions, many=True)
+        return Response(serialized.data, status=status.HTTP_200_OK)
 
     @exception_catcher
     def delete(self, request):
-        pass
+        user_id = request.data['user_id']
+        user = User.objects.filter(user_id=user_id)[0]
+        company_id = user.company_id
+        transactions = Transaction.objects.filter(company_id=company_id)
+        transactions.delete()
+        return Response({"message": f"Transactions of user_id: {user_id} deleted"}, status=status.HTTP_200_OK)
